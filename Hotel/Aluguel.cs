9acc8132daf35ffef96537041c4b34104adae6ff
thiagoatf@hotmail.com <thiagoatf@hotmail.com>
2021-10-06 19:31:47 -0300

    Add project files.

---
+++ Hotel/Aluguel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hotel
+{
+    class Aluguel
+    {
+        //criando as propriedades
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int Quartos { get; set; }
+
+        //criando construtor vazio
+        public Aluguel()
+        {
+        }
+
+        //criando construtor personalizado
+        public Aluguel(string nome, string email, int quartos)
+        {
+            Nome = nome;
+            Email = email;
+            Quartos = quartos;
+        }
+
+        //conversão para string
+        public override string ToString()
+        {
+            return Quartos + ": " + Nome + ", " + Email;
+        }
+    }
+}
---
+++ Hotel/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Hotel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Quantos quartos estão alugados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            //criando um vetor de 10 da classe alguel
+            Aluguel[] vet = new Aluguel[10];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Hóspede #" + (i+1) + ";");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.WriteLine();
+                Console.Write("Quarto: ");
+                int quartos = int.Parse(Console.ReadLine());
+
+                //instaciando as propriedades no lugar dos quartos escolhidos pelo usuário
+                vet[quartos] = new Aluguel(nome, email, quartos);
+
+                Console.WriteLine("---------------------------");
+
+               
+            }
+
+            Console.WriteLine("Quartos ocupados:");
+
+            //for para conferir qual quarto está vazio e qual está ocupado
+            for (int i = 0; i < 10; i++)
+            {
+                if (vet[i] != null)
+                {
+                    Console.WriteLine(vet[i]);
+
+                }
+            }
+        }
+    }
+}
